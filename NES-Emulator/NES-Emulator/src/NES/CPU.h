92d565ed3e69b441c817e0a96044691b7ada6c0a
BanerjeeRaunak99 <banerjeeraunak97@gmail.com>
2020-11-07 14:55:01 +0530

    Added basic cpu registers

--- NES-Emulator/NES-Emulator/src/NES/CPU.h
+++ NES-Emulator/NES-Emulator/src/NES/CPU.h
@@ -1,6 +1,7 @@
 #pragma once
 
 // TODO: Implement CPU functionality
+#include <stdint.h>
 
 namespace NES
 {
@@ -12,6 +13,26 @@ namespace NES
 
 	private:
 		[[nodiscard]] constexpr bool get() { return false; }
+
+		/*memory 2K*/
+		uint8_t memory[2048];
+
+
+		/*general purpose registers*/
+		uint8_t a;
+		uint8_t x;
+		uint8_t y;
+		
+		/*status register*/
+		uint8_t p;
+
+		/*Stack pointer*/
+		uint8_t sp;
+
+		/* Program counter */
+		uint16_t pc;
+
+
 	};
 }
 
